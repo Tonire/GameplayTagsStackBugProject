218cd35f97c1738e4db6b5a47424623b78c69634
Tonire <tonire.rebollo@gmail.com>
2025-03-26 17:20:44 +0100

    Added a debug widget

--- Source/GameplayTagsBugEditor.Target.cs
+++ Source/GameplayTagsBugEditor.Target.cs
@@ -9,7 +9,7 @@ public class GameplayTagsBugEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("GameplayTagsBug");
 	}
 }
